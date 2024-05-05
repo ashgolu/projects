b36c86b3dcf91ed6614133a11d3f566fc628561e
ashgolu <sharkoji1103@gmail.com>
2024-05-05 20:44:09 +0530

    jwt auth with cookies

--- server/index.js
+++ server/index.js
@@ -1,32 +1,61 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const cors=require("cors");
-const UserModel=require("./models/user")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("./models/user")
+// cookie parser module
+const cookieParser = require("cookie-parser");
+const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true 
+};
 
-const app= express();
+// middleware
+const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+// using bcrypt for password encryption
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 
 mongoose.connect("mongodb://127.0.0.1:27017/user")
 
 // api for login
-app.post("/login",(req,res)=>{
-    const {email,password}=req.body;
-        UserModel.findOne({email:email})
-        .then(user=>{
-            if(user){
-                if(user.password===password){
-                    res.json("success")
-                }else{
-                    res.json({message:"incorrect password"})
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    UserModel.findOne({ email: email })
+        .then(async user => {
+            if (user) {
+                const isPasswordCorrect = await bcrypt.compare(password, user.password);
+                if (isPasswordCorrect) {
+                    const sessionToken=jwt.sign({id: user._id},"secretkey",{expiresIn: "1h"})
+                    user.token=sessionToken;
+                    user.password=undefined;
+                    const options = {
+                        expires: new Date(Date.now() + 1000),
+                        httpOnly: true,
+                        sameSite: 'None',
+                        secure: true,
+                        domain: 'localhost',
+                        path: '/'
+                      };
+                    res.status(200).cookie("token", sessionToken, options).json({success:true,sessionToken,user,options})
+                    console.log(sessionToken);
+                   
+                } else {
+                        res.status(400).send({ message: "incorrect password" })
                 }
-            }else{
-                res.json({message:"user not registered"})
+            } else {
+                res.status(400).send({ message: "user not registered" })
+                console.log("user not registered");
             }
-        })})
+        })
+})
 
-    //api for register
-    app.post("/register", async (req, res) => {
+//api for register
+app.post("/register", async (req, res) => {
     const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     const { firstname, lastname, email, password } = req.body;
@@ -34,17 +63,18 @@ app.post("/login",(req,res)=>{
     try {
         const existingUser = await UserModel.findOne({ email });
         if (!existingUser) {
-            if(req.body.email.match(emailRegex) && req.body.password.match(passwordRegex)){
-             const user = await UserModel.create(req.body);
+            const encryptedPassword = await bcrypt.hash(password, 10);
+            if (req.body.email.match(emailRegex) && req.body.password.match(passwordRegex)) {
+                const user = await UserModel.create({ firstname, lastname, email, password: encryptedPassword });
                 console.log("satified");
                 return res.json(user);
             }
-        }  
+        }
     } catch (err) {
         res.json({ message: "An error occurred", error: err });
     }
 });
 
-    app.listen(3001,()=>{
-        console.log("sever is running");
-    })
+app.listen(3001, () => {
+    console.log("sever is running");
+})
--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,8 @@ const UserSchema= new mongoose.Schema({
     firstname:String,
     lastname:String,
     email:String,
-    password:String
+    password:String,
+    token: String
 })
 
 const UserModel= mongoose.model("user",UserSchema)
